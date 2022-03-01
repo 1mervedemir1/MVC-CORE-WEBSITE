dd0b257cbdd75edd29ef75c94f626626b139c72d
1mervedemir1 <merveddemir98@gmail.com>
2022-03-01 15:46:09 +0300

    MVC Core Website

---
+++ MvcCoreDemo/BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,57 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+
+        EFCategoryRepo eFCategoryRepo;
+
+        public CategoryManager()
+        {
+            eFCategoryRepo = new EFCategoryRepo();
+        }
+
+        public void CategoryAdd(Category category)
+        {
+            eFCategoryRepo.Insert(category);
+
+            //if (category.CategoryName != "" && category.CategoryDescription != "" && category.CategoryName.Length>= 5 && category.CategoryStatus== true)
+            //{
+            //    categoryRepository.AddCategory(category);
+            //}
+            //else
+            //{
+            //    //hata mesajı , fluent validation
+
+            //}
+        }
+
+        public void CategoryDelete(Category category)
+        {
+            eFCategoryRepo.Delete(category);
+        }
+
+        public void CategoryUpdate(Category category)
+        {
+            eFCategoryRepo.Update(category);
+        }
+
+        public Category GetById(int id)
+        {
+            return eFCategoryRepo.GetById(id);
+        }
+
+        public List<Category> GetList()
+        {
+            return eFCategoryRepo.GetListAll();
+        }
+    }
+}
---
+++ MvcCoreDemo/DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T: class //entity parametresi aldık ve bir sınıfa ait tüm değerleri kullanacak demek
+    {
+        void Insert(T t);
+        void Delete(T t);
+        void Update(T t);
+        List<T> GetListAll();
+        T GetById(int id);
+    }
+}
